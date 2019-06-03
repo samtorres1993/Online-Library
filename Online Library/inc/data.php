b62b8dfcbc6ab0018f62c8ad52785abb86e077c2
samtorres1993 <43243761+samtorres1993@users.noreply.github.com>
2019-06-03 18:12:06 -0400

    Add files via upload

---
+++ Online Library/inc/data.php
@@ -0,0 +1,163 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "The Catcher in the Rye",
+	"img" => "img/media/catcher.jpg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 1951,
+    "category" => "Books",
+    "authors" => [
+        "J.D. Salinger"
+    ],
+    "publisher" => "Little, Brown Company",
+    "isbn" => '9788373595552'
+];
+$catalog[102] = [
+    "title" => "The Great Gatsby",
+    "img" => "img/media/gatsby.jpg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 1925,
+    "category" => "Books",
+    "authors" => [
+        "F. Scott Fitzgerald"
+    ],
+    "publisher" => "Scribner",
+    "isbn" => '9781505612844'
+];
+$catalog[103] = [
+    "title" => "Harry Potter and the Sorcerer's Stone",
+    "img" => "img/media/potter.jpg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 1997,
+    "category" => "Books",
+    "authors" => [
+        "J.K. Rowling"
+    ],
+    "publisher" => "Arthur A. Levine/ Scholastic",
+    "isbn" => '9781781108765'
+];
+$catalog[104] = [
+    "title" => "The Fault In Our Stars",
+    "img" => "img/media/stars.jpg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 2012,
+    "category" => "Books",
+    "authors" => [
+        "John Green"
+    ],
+    "publisher" => "Dutton Books",
+    "isbn" => '9783844911275'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Star Wars: Revenge of the Sith",
+    "img" => "img/media/sith.jpg",
+    "genre" => "Sci-Fi,Fantasy",
+    "format" => "DVD",
+    "year" => 2005,
+    "category" => "Movies",
+    "director" => "George Lucas",
+    "writers" => [
+        "George Lucas",
+    ],
+    "stars" => [
+        "Ewan McGregor",
+        "Hayden Christensen",
+        "Natalie Portman",
+    ]
+];
+$catalog[202] = [
+    "title" => "Star Wars: A New Hope",
+    "img" => "img/media/newhope.jpg",
+    "genre" => "Sci-Fi,Fantasy",
+    "format" => "DVD",
+    "year" => 1977,
+    "category" => "Movies",
+    "director" => "George Lucas",
+    "writers" => [
+        "George Lucas",
+    ],
+    "stars" => [
+			"Mark Hamill",
+			"Carrie Fisher",
+			"Harrison Ford"
+    ]
+];
+$catalog[203] = [
+    "title" => "Star Wars: The Empire Strikes Back",
+    "img" => "img/media/empire.jpg",
+    "genre" => "Sci-Fi,Fantasy",
+    "format" => "DVD",
+    "year" => 1980,
+    "category" => "Movies",
+    "director" => "George Lucas",
+    "writers" => [
+        "George Lucas",
+    ],
+    "stars" => [
+			"Mark Hamill",
+			"Carrie Fisher",
+			"Harrison Ford"
+    ]
+];
+$catalog[204] = [
+    "title" => "Star Wars: Return of the Jedi",
+		"img" => "img/media/jedi.jpg",
+	  "genre" => "Sci-Fi,Fantasy",
+	  "format" => "DVD",
+	  "year" => 1983,
+	  "category" => "Movies",
+	  "director" => "George Lucas",
+	  "writers" => [
+	      "George Lucas"
+     ],
+	  "stars" => [
+	      "Mark Hamill",
+	      "Carrie Fisher",
+	      "Harrison Ford"
+	  ]
+	];
+//Music
+$catalog[301] = [
+	"title" => "Three Cheers For Sweet Revenge",
+	"img" => "img/media/revenge.jpg",
+	"genre" => "Emo",
+	"format" => "CD",
+	"year" => 2004,
+	"category" => "Music",
+	"artist" => "My Chemical Romance"
+];
+
+$catalog[302] = [
+	"title" => "The Black Parade",
+	"img" => "img/media/blackparade.jpg",
+	"genre" => "Emo",
+	"format" => "CD",
+	"year" => 2006,
+	"category" => "Music",
+	"artist" => "My Chemical Romance"
+];
+$catalog[303] = [
+	"title" => "Dude Ranch",
+	"img" => "img/media/duderanch.jpg",
+	"genre" => "Pop Punk",
+	"format" => "CD",
+	"year" => 1997,
+	"category" => "Music",
+	"artist" => "Blink-182"
+];
+$catalog[304] = [
+	"title" => "Dookie",
+	"img" => "img/media/dookie.jpg",
+	"genre" => "Pop Punk",
+	"format" => "CD",
+	"year" => 1994,
+	"category" => "Music",
+	"artist" => "Green Day"
+];
+?>
